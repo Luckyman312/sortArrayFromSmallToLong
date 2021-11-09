792f8374ca9149129a8a16a469eebeeee74989fa
Luckyman312 <shbahtiyar@gmail.com>
2021-11-09 21:04:31 +0600

    updated

--- sortArrayFromSmallToLong/main.swift
+++ sortArrayFromSmallToLong/main.swift
@@ -1,11 +1,25 @@
-//
-//  main.swift
-//  sortArrayFromSmallToLong
-//
-//  Created by Baktyiar Shamyrbaev on 9/11/21.
-//
+
 
 import Foundation
 
-print("Hello, World!")
+var array = ["312321", "321", "32", "2", "321321321321321"]
+var arrayResult: [String] = []
 
+var itemResult: String = String()
+var itemIndext: Int = 0
+
+while array.count != 0 {
+    for (index, item) in array.enumerated() {
+        if itemResult.count < item.count {
+            itemResult = item
+            itemIndext = index
+        }
+    }
+    
+    array.remove(at: itemIndext)
+    arrayResult.insert(itemResult, at: 0)
+    itemResult = String()
+    itemIndext = 0
+}
+
+print(arrayResult)
